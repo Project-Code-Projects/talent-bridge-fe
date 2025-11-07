045c04545cc078d5c76d350376e95ef74b9b5f1c
Fiayz <sifathhossain115@gmail.com>
2025-11-07 17:17:30 +0600

    Feat: gradient like effect same as landing page provided

--- src/pages/JobsPage.tsx
+++ src/pages/JobsPage.tsx
@@ -1,5 +1,12 @@
 export default function Jobs() {
   return (
-    <section className="relative isolate overflow-hidden pt-28 md:pt-32"></section>
+    <div className="min-h-screen bg-white">
+      <section className="relative isolate overflow-hidden pt-28 md:pt-32">
+        <div className="pointer-events-none absolute inset-0 md:pt-32">
+          <div className="absolute -top-40 right-0 h-72 w-72 rounded full blur-3xl bg-blue-100" />
+          <div className="absolute -top-40 right-0 h-72 w-72 rounded full blur-3xl bg-blue-100" />
+        </div>
+      </section>
+    </div>
   );
 }
